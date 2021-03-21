e280d2c67a3357121c3ad4ccf485ef8e710af16e
kutsyk-neocles <vasyl.kutsyk@neocles.io>
2021-03-21 15:17:17 +0100

    Added release chooser

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -11,7 +11,6 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 import { Button, Checkbox, Chip, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Input, InputLabel, ListItemText, MenuItem, Paper, Select, Tab, Tabs, TextField, Toolbar, Typography } from "@material-ui/core";
 import VersionCard from "../version-card/version-card";
-
 import * as azdev from "azure-devops-node-api";
 import { AzureDevOpsProjectId, OrgUrl } from "../azure-devops-values";
 import * as ReleaseApi from 'azure-devops-node-api/ReleaseApi';
@@ -19,14 +18,11 @@ import * as ReleaseInterfaces from 'azure-devops-node-api/interfaces/ReleaseInte
 import { getTenantsReleasesForDefinition } from "../azure-devops-service";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Autocomplete } from "@material-ui/lab";
-import * as _ from "lodash";
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import TuneIcon from '@material-ui/icons/Tune';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import { TreeViewComponent } from "@syncfusion/ej2-react-navigations";
-import { ContactlessOutlined } from "@material-ui/icons";
-
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
+import { ProgressButtonComponent, SpinSettingsModel, AnimationSettingsModel } from '@syncfusion/ej2-react-splitbuttons';
+import * as _ from "lodash";
 
 function TabPanel(props: any) {
   const { children, value, index, ...other } = props;
@@ -54,17 +50,6 @@ function a11yProps(index: any) {
     'aria-controls': `simple-tabpanel-${index}`,
   };
 }
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
 
 function getReleasesFolderStructure(allReleases: any, releasesFolders: any) {
   for (let release of allReleases) {
@@ -177,22 +162,23 @@ function mergeDeep(children: any) {
 class Index extends React.Component<{}, any> {
   checkedNodes: any;
   showCheckBox: boolean = true;
+  treeRef: any;
 
   constructor(props: {}) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
     this.handleEnvTabChange = this.handleEnvTabChange.bind(this);
     this.handleOpenReleasesDialog = this.handleOpenReleasesDialog.bind(this);
-    this.handleCloseReleaseDIalog = this.handleCloseReleaseDIalog.bind(this);
+    this.handleCloseReleaseDialog = this.handleCloseReleaseDialog.bind(this);
     this.handleReleaseNodeCheck = this.handleReleaseNodeCheck.bind(this);
+    this.renderReleaseChooserDialog = this.renderReleaseChooserDialog.bind(this);
 
     this.state = {
       value: 0,
       loading: true,
+      saving: false,
       chosenReleases: [],
-      releasesNames: [],
       open: false,
-      resourcesField: {}
+      releasesForChooser: {}
     };
   }
 
@@ -202,35 +188,37 @@ class Index extends React.Component<{}, any> {
     });
   };
 
-  handleCloseReleaseDIalog = () => {
-    this.setState({
-      open: false
-    });
-  };
-
-
-  handleChange = async (event: any, releases: any) => {
-    this.setState({
-      chosenReleases: releases
-    });
-
+  handleCloseReleaseDialog = async () => {
     if (this.state.releaseApiObject) {
+      this.setState({
+        saving: true
+      });
       let processedDeployments = {};
       let deployments = {};
-      for (let r of releases) {
+      for (let r of this.state.chosenReleases) {
         const releaseAzure: ReleaseInterfaces.ReleaseDefinition[] = await this.state.releaseApiObject.getReleaseDefinitions(AzureDevOpsProjectId, r.name);
         const deployment: any = await getTenantsReleasesForDefinition(releaseAzure, this.state.releaseApiObject);
         deployments = _.merge(processedDeployments, deployment);
       }
 
       this.setState({
-        deployments: deployments
+        deployments: deployments,
+        open: false,
+        saving: false
       });
     }
   };
 
   handleReleaseNodeCheck(args: any) {
-    console.log(`this.checkedNodes: ${this.checkedNodes}`);
+    let releases: Array<any> = [];
+    if (this.state.releasesForChooser.dataSource) {
+      for (let i of this.treeRef.checkedNodes) {
+        releases.push(this.state.releasesForChooser.dataSource[i]);
+      }
+      this.setState({
+        chosenReleases: releases
+      });
+    }
   }
 
   handleEnvTabChange(event: any, value: any) {
@@ -249,31 +237,53 @@ class Index extends React.Component<{}, any> {
 
     this.setState({ releaseApiObject: releaseApiObject, token: accessToken });
 
-    let releasesNames: any = [];
-    let releaseChooser: any = [];
-    let i: number = 0;
-    const paths: Array<string> = [];
-    for (let release of allReleases) {
-      let relPath: string = release?.path?.substring(1) ?? "";
-      paths.push(relPath);
-    }
+    const paths: Array<string> = allReleases.map(r => r?.path?.substring(1) ?? "");
     const releasesFolders = mergeDeep(paths.map(genTree).map(([e]) => e));
     getReleasesFolderStructure(allReleases, releasesFolders);
 
     let chooserIndex = 0;
-
+    let releaseChooser: any = [];
     for (let f of releasesFolders) {
       let structure = getReleasesChooserStructure(chooserIndex, null, f);
       chooserIndex += structure.length;
       releaseChooser = releaseChooser.concat(structure);
     }
-    console.log(releaseChooser);
+
     this.setState({
-      releasesNames: releasesNames,
-      resourcesField: { dataSource: releaseChooser, id: 'id', parentID: 'pid', text: 'name', hasChildren: 'hasChild' },
+      releasesForChooser: { dataSource: releaseChooser, id: 'id', parentID: 'pid', text: 'name', hasChildren: 'hasChild' },
       loading: false
     });
+  }
 
+  renderReleaseChooserDialog() {
+    let dialogBody = (this.state.saving)
+      ? (
+        <Grid
+          container
+          alignItems="center"
+          direction="column">
+          <CircularProgress />
+          <Typography>Saving...</Typography>
+        </Grid>
+      )
+      : (<TreeViewComponent
+        ref={treeRef => this.treeRef = treeRef as TreeViewComponent}
+        fields={this.state.releasesForChooser}
+        showCheckBox={this.showCheckBox}
+        nodeChecked={this.handleReleaseNodeCheck}
+        checkedNodes={this.checkedNodes} />);
+
+    return (<Dialog onClose={this.handleCloseReleaseDialog} aria-labelledby="customized-dialog-title" open={this.state.open}>
+      <DialogTitle id="customized-dialog-title">
+        Choose releases
+    </DialogTitle>
+      <DialogContent dividers style={{ width: '480px' }}>
+        {dialogBody}
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus color="primary" onClick={this.handleCloseReleaseDialog}>Save</Button>
+      </DialogActions>
+    </Dialog >);
   }
 
   public render(): JSX.Element {
@@ -304,58 +314,12 @@ class Index extends React.Component<{}, any> {
           <Paper variant="outlined">
             <Grid container spacing={1}>
               <Grid item xs={12} sm={6} md={6}>
-                {/* <Autocomplete
-                  multiple
-                  id="releases-select"
-                  groupBy={(option) => option.path}
-                  disableCloseOnSelect
-                  options={this.state.releasesNames.sort((a: any, b: any) => -b.path.localeCompare(a.path))}
-                  getOptionLabel={(option) => option.name}
-                  onChange={this.handleChange}
-                  filterSelectedOptions
-                  renderOption={(option, { selected }) => (
-                    <React.Fragment>
-                      <Checkbox
-                        icon={icon}
-                        checkedIcon={checkedIcon}
-                        style={{ marginRight: 8 }}
-                        checked={selected}
-                      />
-                      {option.name}
-                    </React.Fragment>
-                  )}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      variant="outlined"
-                      label="Releases"
-                      placeholder="Please choose"
-                    />
-                  )}
-                /> */}
-              </Grid>
-              <Grid item xs={12} sm={6} md={6}>
-                <Button variant="outlined" color="primary" onClick={this.handleOpenReleasesDialog}>
+                <Button size="small" color="primary"
+                  onClick={this.handleOpenReleasesDialog}
+                  startIcon={<TuneIcon />}>
                   Releases
                 </Button>
-
-                <Dialog onClose={this.handleCloseReleaseDIalog} aria-labelledby="customized-dialog-title" open={this.state.open}>
-                  <DialogTitle id="customized-dialog-title">
-                    Choose releases
-                  </DialogTitle>
-                  <DialogContent dividers style={{ width: '480px' }}>
-                    <TreeViewComponent
-                      fields={this.state.resourcesField}
-                      showCheckBox={this.showCheckBox}
-                      nodeChecked={this.handleReleaseNodeCheck}
-                      checkedNodes={this.checkedNodes} />
-                  </DialogContent>
-                  <DialogActions>
-                    <Button autoFocus onClick={this.handleCloseReleaseDIalog} color="primary">
-                      Save changes
-                    </Button>
-                  </DialogActions>
-                </Dialog>
+                {this.renderReleaseChooserDialog()}
               </Grid>
             </Grid>
           </Paper>
